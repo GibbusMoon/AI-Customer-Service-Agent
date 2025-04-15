c2f7533b36fe11eea7f79fce398edfbc4d42a083
Micah Moon <108433029+GibbusMoon@users.noreply.github.com>
2025-04-15 15:22:50 -0500

    Update server.js
    Updated PORT

--- server.js
+++ server.js
@@ -111,4 +111,7 @@ app.get("/api/query", async (req, res) => {
 
 //--Gemeni API Tracking--//
 
-app.listen(parseInt(process.env.PORT), () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(Server is running on port ${PORT});
+});
