f5e000ed1c39e5e510122bc8543219ac207bd62b
Micah Moon <108433029+GibbusMoon@users.noreply.github.com>
2025-04-15 15:25:13 -0500

    Update server.js
    Updated PORT correctly

--- server.js
+++ server.js
@@ -113,5 +113,5 @@ app.get("/api/query", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(Server is running on port ${PORT});
+  console.log('Server is running on port ${PORT}');
 });
