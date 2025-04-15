5a297ed72b72b615b2ecebf989998991b222c39f
Micah Moon <108433029+GibbusMoon@users.noreply.github.com>
2025-04-15 16:59:22 -0500

    Update ChatBox.vue

--- frontend/src/views/ChatBox.vue
+++ frontend/src/views/ChatBox.vue
@@ -73,7 +73,7 @@
       
         this.messages.push({ text: this.userInput, sender: "user" });
 
-        const url = "http://" + process.env.VUE_APP_HOST_IP + "/api/query";
+        const url = process.env.VUE_APP_HOST_IP + "/api/query";
                 
 
         const response = await axios.get(url, { 
